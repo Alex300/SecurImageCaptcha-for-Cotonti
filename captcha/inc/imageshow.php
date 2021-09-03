7ca42875ab0b6ff914f52adaa696cb79c7d5a224
Alexey Kalnov <kalnovalexey@yandex.ru>
2021-09-03 09:44:04 +0300

    Compatibility with PHP 8.0  in progress

--- captcha/inc/imageshow.php
+++ captcha/inc/imageshow.php
@@ -79,8 +79,11 @@ if($font == 'anorexia.ttf'){
 //$img->ttf_file        = './Quiff.ttf';
 //$img->captcha_type    = Securimage::SI_CAPTCHA_MATHEMATIC; // show a simple math problem instead of text
 $img->case_sensitive  = false;                               // true to use case sensitve codes - not recommended
-$img->image_height    = 70;                                  // height in pixels of the image
-$img->image_width     = intval($img->image_height * M_E);          // a good formula for image size based on the height
+
+// May be turn it on again?
+//$img->image_height    = 70;                                  // height in pixels of the image
+//$img->image_width     = intval($img->image_height * M_E);    // a good formula for image size based on the height
+
 //$img->perturbation    = .75;                               // 1.0 = high distortion, higher numbers = more distortion
 //$img->image_bg_color  = new Securimage_Color("#0099CC");   // image background color
 //$img->text_color      = new Securimage_Color("#EAEAEA");   // captcha text color
